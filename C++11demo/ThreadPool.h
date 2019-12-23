9b11bde3ca9a9f827cf77531a18517b53ae3240a
kelinlang <kelinlang@163.com>
2019-12-23 23:46:18 +0800

    dd

--- C++11demo/ThreadPool.h
+++ C++11demo/ThreadPool.h
@@ -69,12 +69,14 @@ template<typename F, typename ...Args>
 inline auto ThreadPool::enqueue(F&& f, Args&& ...args) -> std::future<typename std::result_of<F(Args ...)>::type>
 {
 	using return_type = typename std::result_of<F(Args ...)>::type;
-	auto task = std::make_shared<std::packaged_task<return_type()>>(
+
+	auto task = std::make_shared< std::packaged_task<return_type()> >(
 		std::bind(std::forward<F>(f), std::forward<Args>(args)...)
 		);
 
-	std::future<return_type> res = task->get();
+	std::future<return_type> res = task->get_future();
 	{
+		std::unique_lock<std::mutex> lock(queue_mutex);
 		if (stop) {
 			throw std::runtime_error("enqueue on stopped ThreadPool");
 		}
@@ -84,5 +86,5 @@ inline auto ThreadPool::enqueue(F&& f, Args&& ...args) -> std::future<typename s
 	}
 	condition.notify_one();
 
-	return std::future<typename std::result_of<F(Args ...)>::type>();
+	return res;
 }
